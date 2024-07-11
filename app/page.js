9a9443d4fa1e3277331defdfbdf462f54ad7a7d4
Borys Shkliar <dev.bosk@gmail.com>
2024-07-11 12:40:17 +0200

    init project

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Header from "@/components/header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <p>🔥 Let&apos;s get started! 🔥</p>
+    </main>
+  );
+}
