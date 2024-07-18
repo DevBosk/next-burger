33f28d537f3a61134e37ff2b76d78d15419be09f
Borys Shkliar <dev.bosk@gmail.com>
2024-07-18 15:34:55 +0200

    dynamic routes

---
+++ app/about/[test]/page.js
@@ -0,0 +1,7 @@
+export default function AboutDynamic({ params }) {
+    return (
+        <main>
+            {params.test}
+        </main>
+    );
+}
--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function AboutPage() {
     return (
         <main>
             <h1>About Us</h1>
+            <h2><Link href='/about/a-1'>A1</Link></h2>
+            <h2><Link href='/about/a-2'>A2</Link></h2>
         </main>
     )
 }
