988afa4396afb234fffd2a9c0a4141305465deb4
Borys Shkliar <dev.bosk@gmail.com>
2024-07-22 10:55:00 +0200

    meals page

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,21 +3,30 @@ import headerLogo from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
     return <>
         <MainHeaderBackground />
         <Link className={classes.logo} href='/'>
-            <Image src={headerLogo.src} alt="A plate with food on it" width={1024} height={1024} priority />
+            <Image src={headerLogo} alt="A plate with food on it" priority />
             NextLevel Food
 
             <nav className={classes.nav}>
                 <ul>
                     <li>
-                        <Link href="/meals">Meals</Link>
+                        <NavLink 
+                            href='/meals'
+                        >
+                            Meals
+                        </NavLink>
                     </li>
                     <li>
-                        <Link href="/community">Foodies Community</Link>
+                        <NavLink 
+                            href='/community'
+                        >
+                            Foodies Community
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
