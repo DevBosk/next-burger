34e3eb42227deae1d8938ceee11345a0b46461cb
Borys Shkliar <dev.bosk@gmail.com>
2024-07-22 11:30:10 +0200

    added db table and dummy data for meals

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -2,9 +2,9 @@ import MealItem from './meal-item';
 import classes from './meals-grid.module.css';
 
 export default function MealsGrid( { meals }) {
-    return <ul>
+    return <ul className={classes.meals}>
         {meals.map(meal => <li key={meal.id}>
-            <MealItem {...meals} />
+            <MealItem {...meal} />
         </li>)}
     </ul>
 }
